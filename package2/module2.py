78a305f83964817078d9d2ec0728f2ec5c378ca3
vickieshih <vickieshih@pchome.com.tw>
2018-03-29 20:57:25 +0800

    git

---
+++ package2/module2.py
@@ -0,0 +1,2 @@
+def function2():
+    print("function 2")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = '295homework',
+    packages = ['package1', 'package2'],
+    version = '0.2',
+    description = 'Python程式設計295期_homework',
+    author = 'vickieshih',
+    author_email = 'vickieshih@pchome.com.tw',
+    url = 'https://github.com/vickieshih/py_project_1.git',
+    download_url = 'https://github.com/vickieshih/py_project_1/tarball/0.2',
+    keywords = ['homework']
+)
